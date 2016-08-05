cdebbd1039acb2cc0f7b93cccb9d1f4407e977fa
Jack Savich <jack.savich@gmail.com>
2016-08-05 12:02:39 +0300

    register facades

--- src/Providers/ApiServiceProvider.php
+++ src/Providers/ApiServiceProvider.php
@@ -3,6 +3,8 @@ namespace Savich\ApiSkeleton\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Tymon\JWTAuth\Providers\JWTAuthServiceProvider;
+use Savich\ApiSkeleton\Response\ResponseApiFacade;
+use Illuminate\Foundation\AliasLoader;
 
 class ApiServiceProvider extends ServiceProvider
 {
@@ -17,5 +19,10 @@ class ApiServiceProvider extends ServiceProvider
         $this->app->register(ResponseServiceProvider::class);
         $this->app->register(JWTAuthServiceProvider::class);
         $this->app->register(ApiEventServiceProvider::class);
+
+        $loader = AliasLoader::getInstance();
+        $loader->alias('ResponseApi', ResponseApiFacade::class);
+        $loader->alias('JWTAuth', \Tymon\JWTAuth\Facades\JWTAuth::class);
+        $loader->alias('JWTFactory', \Tymon\JWTAuth\Facades\JWTFactory::class);
     }
 }
