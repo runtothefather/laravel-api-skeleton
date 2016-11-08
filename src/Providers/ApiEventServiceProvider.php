d0bddc5c5b9df92900c4094f7e1134b6bb2c4515
IlyaSavich <ilia.savich.97@gmail.com>
2016-11-08 18:34:18 +0300

    aaa

--- src/Providers/ApiEventServiceProvider.php
+++ src/Providers/ApiEventServiceProvider.php
@@ -2,6 +2,7 @@
 namespace Savich\ApiSkeleton\Providers;
 
 use Event;
+use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Contracts\Foundation\Application;
 use Savich\ApiSkeleton\Response\ResponseApi;
@@ -22,11 +23,12 @@ class ApiEventServiceProvider extends ServiceProvider
     /**
      * Register any other events for your application.
      *
+     * @param  \Illuminate\Contracts\Events\Dispatcher  $events
      * @return void
      */
-    public function boot()
+    public function boot(DispatcherContract $events)
     {
-        parent::boot();
+        parent::boot($events);
 
         Event::listen('tymon.jwt.absent', function () {
             return $this->response->send([], ['Token not provided'], ResponseApi::STATUS_NOT_PROVIDED);
